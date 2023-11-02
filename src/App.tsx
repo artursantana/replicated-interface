b13861ec0282369843ab5996890be5b07fcc35fb
artursantana <artursantana843@gmail.com>
2023-11-02 00:38:17 +0000

    Add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import './App.scss'
 function App() {
   return (
     <main>
-      <h1>my app</h1>
+      <header className='header-container'>
+        <div className='logo-container'>
+          <img src='./logo-img.svg'/>
+          <div className='logo-text'>
+            <span className='logo-text-bold'>summerly</span>
+            <span className='logo-text-regular'>media</span>
+          </div>
+          </div>
+        <nav className="menu-options">
+          <a href="#">What We Offer</a>
+          <a href="#">How it Works</a>
+          <a href="#">Portifolio</a>
+        </nav>
+        <button>Contact us</button>
+
+      </header>
     </main>
   )
 }
