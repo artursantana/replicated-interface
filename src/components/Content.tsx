885ed12785687bb93901eeefbe4b40c30e30eeb6
artursantana <artursantana843@gmail.com>
2023-11-03 08:52:08 +0000

    second validation

---
+++ src/components/Content.tsx
@@ -0,0 +1,21 @@
+
+
+export const Content = () => {
+  return (
+    <div className="content-container">
+        <h1>Let's grow your social presence</h1>
+        <div className="bubble_target">
+        🚀
+        </div>
+        <div className="bubble_target">
+        🎯
+        </div>
+        <div className="bubble_target">
+        😵
+        </div>
+        <div className="bubble_target">
+        🍩
+        </div>
+    </div>
+  )
+}
