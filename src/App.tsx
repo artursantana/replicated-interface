15a833260ced3f9638c47d7589f160610123f540
artursantana <artursantana843@gmail.com>
2023-11-03 00:07:08 +0000

    Add new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import './App.scss'
+import '../src/App.scss'
 
-function App() {
+import Home from './components/Home'
+
+export const App = () => {
   return (
-    <main>
-      <header className='header-container'>
-        <div className='logo-container'>
-          <img src='./logo-img.svg'/>
-          <div className='logo-text'>
-            <span className='logo-text-bold'>summerly</span>
-            <span className='logo-text-regular'>media</span>
-          </div>
-          </div>
-        <nav className="menu-options">
-          <a href="#">What We Offer</a>
-          <a href="#">How it Works</a>
-          <a href="#">Portifolio</a>
-        </nav>
-        <button>Contact us</button>
-
-      </header>
-    </main>
+    <div>
+      <Home />
+    </div>
   )
 }
-
-export default App
